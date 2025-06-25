4eaa8c6716c15272ec5c927c7373df63f3050354
Ndzenyuy <ndzenyuyjones@gmail.com>
2025-06-25 01:27:35 +0100

    geting monguri set up

--- nodeapi/server.js
+++ nodeapi/server.js
@@ -6,7 +6,8 @@ const cors = require("cors");
 const colors = require("colors");
 
 const mongooseURI = process.env.MONGO_URI || "mongodb://localhost:27017/emart";
-console.log("##############mongo db uri" + MONGO_URI);
+console.log("Mongo URI:", process.env.MONGO_URI);
+console.log("Using Mongoose URI:", mongooseURI);
 
 const userRoutes = require("./routes/user");
 const shopRoutes = require("./routes/shop");
